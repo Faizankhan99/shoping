377a07f3fdae7c6e19cef410991e22669b1abaca
Faizankhan99 <usmani.faizan98@gmail.com>
2022-12-15 13:19:19 +0530

    add

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const { mongoose } = require('mongoose')
+const app = express()
+const Shoping = require("../Routing/shoping")
+const Bookmark=require("../Routing/bookmarks")
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+const cors = require("cors")
+require("dotenv").config()
+const PORT = process.env.PORT || 9000;
+const mongo_url=process.env.mongo_url||""
+
+app.use("/all", Shoping)
+app.use("/bookmark",Bookmark)
+app.get('/', (req, res) => res.send('hello'))
+
+
+app.listen(PORT, async () => {
+    await mongoose.connect(mongo_url)
+    console.log(`server started on port ${PORT}`)
+})
